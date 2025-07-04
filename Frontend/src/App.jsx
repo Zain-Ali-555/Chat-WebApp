c7d7b304be811f701fb3b0f5d6bd9131eb566dbf
Zain Ali <zainchamp5@gmail.com>
2025-07-04 23:52:17 +0500

    7th - [ New section created and message section makes working! ]

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Sample from "./components/sample";
 import OpenedChat from "./components/openedChat";
 import Sidebar from "./components/sidebar";
 import ChatSection from "./components/chatSection";
+import NotCreatedSection from "./components/notCreatedSection";
 
 const App = () => {
   return (
@@ -15,9 +16,10 @@ const App = () => {
         <Sidebar imageUrl="https://images.unsplash.com/photo-1744566917600-57a72a02a6d8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyNXx0b3dKWkZza3BHZ3x8ZW58MHx8fHx8" />
         <ChatSection name="Me" imageUrl="https://plus.unsplash.com/premium_photo-1727343268376-0521b84e43ba?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         <div className="openedChatSection w-[65%] h-full bg-zinc-900 rounded-t-2xl overflow-hidden mr-2 border-2 flex justify-center items-center text-zinc-700">
-          {/* <Sample /> */}
-          <OpenedChat />
-        </div>
+        {/* <Sample /> */}
+        <OpenedChat />
+        {/* <NotCreatedSection /> */}
+      </div>
       </div>
     </>
   );
--- Frontend/src/components/openedChat.jsx
+++ Frontend/src/components/openedChat.jsx
@@ -1,50 +1,47 @@
+import { useRef } from "react";
 import { IoSend } from "react-icons/io5";
-const openedChat = () => {
+const OpenedChat = () => {
+  // let input = document.querySelector(".SendIcon");
+  const inputRef = useRef();
+  let valueGetHandler = () => {
+    console.log("Working!");
+    let value = inputRef.current.value;
+    let newDiv = document.createElement("div");
+    newDiv.classList.add =
+      "message2 max-w-1/2 self-end bg-green-700 text-white py-1 px-2 rounded-md";
+    newDiv.innerHTML = `<h5>${value}</h5>`;
+
+    console.log("Not working!");
+  };
   return (
     <>
       <div className="openedPersonChat w-full flex flex-col  overflow-hidden ">
         <div className="openedTop w-full h-[10vh] p-5 bg-zinc-600 flex items-center gap-5 cursor-pointer hover:bg-zinc-600/90">
-          <div className="w-14 h-14 border-1 border-zinc-700 bg- rounded-full bg-[url(https://plus.unsplash.com/premium_photo-1749836090972-c31e9c878b92?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-no-repeat bg-cover bg-center "></div>
+          <img
+            className="w-14 h-14 border-1 border-zinc-700 rounded-full object-cover"
+            src="https://plus.unsplash.com/premium_photo-1749836090972-c31e9c878b92?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
           <h5 className="text-2xl text-white">Waseem Shazad</h5>
         </div>
         <div className="openedMiddle w-full h-[75vh] flex flex-col gap-2 p-2">
-          <div className="clientAdminChat overflow-y-auto w-full h-full flex flex-col gap-5 p-5">
-            <div className="message1 max-w-1/2 self-start bg-zinc-700 text-white py-1 px-2 rounded-md">
-              <h5>Zain wo project kitna kr lia?</h5>
-            </div>
-            <div className="message2 max-w-1/2 self-end bg-green-700 text-white py-1 px-2 rounded-md">
-              <h5>Hmm kr raha hoo thora thora bss.</h5>
-            </div>
-            <div className="message1 max-w-1/2 self-start bg-zinc-700 text-white py-1 px-2 rounded-md">
-              <h5>mst hai jaldi kr?</h5>
-            </div>
-            <div className="message2 max-w-1/2 self-end bg-green-700 text-white py-1 px-2 rounded-md">
-              <h5>haa g waseem bhai kr raha hoo</h5>
-            </div>
-            <div className="message1 max-w-1/2 self-start bg-zinc-700 text-white py-1 px-2 rounded-md">
-              <h5>
-                aur repo abhi tk ni bnai? me subha se wait kr raha sunday hai.
-              </h5>
-            </div>
-            <div className="message2 max-w-1/2 self-end bg-green-700 text-white py-1 px-2 rounded-md">
-              <h5>
-                waseem bhai wo bnai hai lakin sham ko bnai hai. bss me ne apko
-                collab wala link kab ka send kia hoa hai
-              </h5>
-            </div>
-          </div>
+          <div className="clientAdminChat overflow-y-auto w-full h-full flex flex-col gap-5 p-5"></div>
         </div>
-        <div className="openedBottom w-full h-[10vh] p-5 border-zinc-600 border-t-2 flex justify-center items-center gap-2">
+        <div className="openedBottom w-full h-[10vh] p-5 border-zinc-800 border-t-2 flex justify-center items-center gap-2">
           <input
+            ref={inputRef}
             className="w-full p-2 rounded text-white outline-none "
             type="text"
             placeholder="Enter your message..."
           />
-          <IoSend className="text-5xl cursor-pointer hover:bg-zinc-500 p-1 rounded" />
+          <IoSend
+            onClick={valueGetHandler}
+            className="SendIcon text-5xl cursor-pointer hover:bg-zinc-600 p-1 rounded"
+          />
         </div>
       </div>
     </>
   );
 };
 
-export default openedChat;
+export default OpenedChat;
