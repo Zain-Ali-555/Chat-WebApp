82147525237a5209509d6f1b971949ca16bd2ba9
Waseem-786 <w4w.w4me@gmail.com>
2025-07-05 22:22:56 +0500

    Initializing backend side of the project, created end points for users creation, user getting, and token creation

---
+++ Frontend/src/components/notCreatedSection.jsx
@@ -0,0 +1,11 @@
+const NotCreatedSection = () => {
+  return (
+    <>
+      <div className="EmptySection bg-zinc-900 w-full h-80% rounded grid content-center text-center">
+        <h5 className="text-2xl text-zinc-600">Creation in progress!</h5>
+      </div>
+    </>
+  );
+};
+
+export default NotCreatedSection;
